dc8b134f78c47cadb48b5e0ca5be0ca111f21b61
Luis Acuña <luisandresverdugox@gmail.com>
2023-08-20 14:50:25 -0500

    Subiendo estructura del proyecto

---
+++ transsaction/src/main/java/api/transsaction/TranssactionApplication.java
@@ -0,0 +1,13 @@
+package api.transsaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranssactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranssactionApplication.class, args);
+	}
+
+}
---
+++ transsaction/src/test/java/api/transsaction/TranssactionApplicationTests.java
@@ -0,0 +1,13 @@
+package api.transsaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranssactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
